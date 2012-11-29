1be26145f7b1d19b73d6c867b683024688c4f414
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2012-11-29 19:37:42 +0000

    refactor tests to assert once per function.

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,7 +2,7 @@ import boto
 from boto.s3.bucket import Bucket
 
 
-def empty_bucket(bucket_name):
+def delete_all(bucket_name):
     """Destructive helper."""
     bucket = Bucket(connection=boto.connect_s3(), name=bucket_name)
     for key in bucket.get_all_keys():
--- tests/test_compost.py
+++ tests/test_compost.py
@@ -1,32 +1,46 @@
 from compost import Compost
+from . import helpers
 
-from .helpers import empty_bucket
 
-
-def test_compost():
+def pytest_funcarg__compost(request):
     """
-    Test the very basics of compost management:
-    Synchronise a local directory to the backup location,
-    make sure that the files we expect to have been backed up are available
-    to list and retrieve.
-
-    Connects to an actual S3 bucket (as defined below) using any keys you might have
-    defined in your environment.
+    Returns a Compost object that is guaranteed to be empty.
     """
-
-    compost = Compost(
+    # assumed pre-conditions:
+    #  the bucket test-git-repos-51806ac9a7bba4c653688ff6f18d04f2 exists and is writable.
+    #  /data/git-repos exists and contains some files (details below in actual tests).
+    helpers.delete_all(bucket_name='test-git-repos-51806ac9a7bba4c653688ff6f18d04f2')
+    return Compost(
         directory='/data/git-repos',
         bucket='test-git-repos-51806ac9a7bba4c653688ff6f18d04f2'
     )
 
+"""
+These tests cover the very basics of compost management:
+Synchronise a local directory to the backup location,
+make sure that the files we expect to have been backed up are available
+to list and retrieve.
+
+Connects to an actual S3 bucket (as defined below) using any keys you might have
+defined in your environment.
+"""
+
+
+def test_empty_compost_is_empty(compost):
     assert compost.list() == []
 
+
+def test_turning_a_compost_synchronises_local_files_with_remote_bucket(compost):
     compost.turn()
     known_files = compost.list()
     assert sorted(known_files) == ['desktop-20120828.bundle', 'home-20120828.bundle']
 
+
+def test_read(compost):
+    compost.turn()
     assert compost.read('home-20120828.bundle') == 'heh, what? this is not a binary bundle.\n'
     assert compost.read('desktop-20120828.bundle') == 'nor are we.\n'
 
-    empty_bucket('test-git-repos-51806ac9a7bba4c653688ff6f18d04f2')
+
+def test_list(compost):
     assert compost.list() == []
